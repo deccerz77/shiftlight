9cf82d407a126a4fe4af6db61593b826e3a310d8
Declan Armstrong <declan.armstrong@live.co.uk>
2019-06-26 21:16:38 +0100

    beginning

---
+++ Shift_Light_Configurator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shift_Light_Configurator
+//
+//  Created by Declan Armstrong on 26/06/2019.
+//  Copyright © 2019 Declan Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
